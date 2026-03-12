4db2acc71f5a3805dcf40db4207f3aeb25369d24
Rafaela Merlotto Parrilla <rafaela.parrilla@aluno.senai.br>
2026-03-12 09:40:25 -0300

    Etapa 7 corrigida

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ function showSlide(slideInex) {
 // Função para atualizar a posição do carrossel
 function updateSlidePosition() {
     const slideWidth = slides[0].offsetWidth;
-    carouselSlides.style.transform = ´translateX(-${currenntSlide * slideWidth}px)`;
+    carouselSlides.style.transform = `translateX(-${currenntSlide * slideWidth}px)`;
 }
 
 // Função para avançar para o próximo slide
